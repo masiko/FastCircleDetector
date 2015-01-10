9b4103e787fd04266481a710030dc3e2991f31c7
masiko <masiko@masiko-SVS13A3AJ.(none)>
2015-01-10 22:12:34 +0900

    change CMakeLists, include filename

--- test_main.cpp
+++ test_main.cpp
@@ -1,6 +1,6 @@
-#include "stdio.h"
-#include "cv.h"
-#include "highgui.h"
+#include <stdio.h>
+#include <opencv2/opencv.hpp>
+#include <opencv2/highgui/highgui.hpp>
 #include "FCD.h"
 #include "DownScale.h"
 
